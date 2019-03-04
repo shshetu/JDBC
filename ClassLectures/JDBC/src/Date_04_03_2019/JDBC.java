d92d551243649fcbf22d83a55498236b2fc1e292
shshetu <shshetu2017@gmail.com>
2019-03-04 19:02:55 +0600

    jdbc

---
+++ ClassLectures/JDBC/src/Date_04_03_2019/JDBC.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Date_04_03_2019;
+
+/**
+ *
+ * @author User
+ */
+public class JDBC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
